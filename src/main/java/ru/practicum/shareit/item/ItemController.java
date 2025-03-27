535b94fbcf12b36c84b3fce4dec7c614a847d8cb
amagmail <skynet@skynet.ru>
2025-03-28 01:28:25 +0300

    Sprint 14. Разработка финального проекта

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,44 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import java.util.Collection;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/items")
 public class ItemController {
+
+    private final ItemService itemService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ItemDto create(@RequestHeader("X-Later-User-Id") Long userId, @RequestBody @Valid ItemDto entity) {
+        return itemService.create(userId, entity);
+    }
+
+    @PatchMapping("/{id}")
+    public ItemDto update(@RequestHeader("X-Later-User-Id") Long userId, @PathVariable("id") Long itemId, @RequestBody ItemDto entity) {
+        return itemService.update(userId, itemId, entity);
+    }
+
+    @GetMapping("/{id}")
+    public ItemDto getItem(@RequestHeader("X-Later-User-Id") Long userId, @PathVariable("id") Long itemId) {
+        return itemService.getItem(userId, itemId);
+    }
+
+    @GetMapping
+    public Collection<ItemDto> getItems(@RequestHeader("X-Later-User-Id") Long userId) {
+        return itemService.getItems(userId);
+    }
+
+    @GetMapping("/search")
+    public Collection<ItemDto> getSearch(@RequestHeader("X-Later-User-Id") Long userId, @RequestParam String text) {
+        return itemService.getSearch(userId, text);
+    }
+
 }
---
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -0,0 +1,50 @@
+package ru.practicum.shareit.item;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemMapper;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.storage.ItemStorage;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    private final ItemStorage itemStorage;
+
+    @Override
+    public ItemDto create(Long userId, ItemDto entity) {
+        Item item = itemStorage.create(userId, ItemMapper.toItemModel(entity));
+        return ItemMapper.toItemDto(item);
+    }
+
+    @Override
+    public ItemDto update(Long userId, Long itemId, ItemDto entity) {
+        Item item = itemStorage.update(userId, itemId, ItemMapper.toItemModel(entity));
+        return ItemMapper.toItemDto(item);
+    }
+
+    @Override
+    public ItemDto getItem(Long userId, Long itemId) {
+        Item item = itemStorage.getItem(userId, itemId);
+        return ItemMapper.toItemDto(item);
+    }
+
+    @Override
+    public Collection<ItemDto> getItems(Long userId) {
+        return itemStorage.getItems(userId).stream()
+                .map(ItemMapper::toItemDto)
+                .toList();
+    }
+
+    @Override
+    public Collection<ItemDto> getSearch(Long userId, String text) {
+        return itemStorage.getSearch(userId, text).stream()
+                .map(ItemMapper::toItemDto)
+                .toList();
+    }
+
+}
