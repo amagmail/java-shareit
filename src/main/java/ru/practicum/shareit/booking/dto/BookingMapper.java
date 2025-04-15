ef7c1e1486850b104d11bd092c74b76530a517a5
amagmail <skynet@skynet.ru>
2025-04-15 08:37:05 +0300

    Sprint 15. Исправления по результату проверки

--- src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
@@ -2,6 +2,8 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.experimental.UtilityClass;
 import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.dto.ItemMapper;
+import ru.practicum.shareit.user.dto.UserMapper;
 
 @UtilityClass
 public class BookingMapper {
@@ -11,8 +13,8 @@ public class BookingMapper {
         dto.setId(booking.getId());
         dto.setStart(booking.getStart());
         dto.setEnd(booking.getEnd());
-        dto.setItem(booking.getItem());
-        dto.setBooker(booking.getBooker());
+        dto.setItem(ItemMapper.toItemDto(booking.getItem()));
+        dto.setBooker(UserMapper.toUserDto(booking.getBooker()));
         dto.setStatus(booking.getStatus());
         return dto;
     }
