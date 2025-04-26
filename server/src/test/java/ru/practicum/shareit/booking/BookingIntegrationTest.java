110302fba7419d084a4dadf9ef3118e5df19b3e1
amagmail <skynet@skynet.ru>
2025-04-26 08:53:40 +0300

    Sprint 16. Разработка финального проекта (Правки)

--- server/src/test/java/ru/practicum/shareit/booking/BookingIntegrationTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/BookingIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import ru.practicum.shareit.ShareItServer;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingRequestDto;
+import ru.practicum.shareit.booking.enums.BookingStatus;
 import ru.practicum.shareit.item.ItemController;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.user.UserController;
@@ -88,6 +89,34 @@ public class BookingIntegrationTest {
         BookingDto b1 = bookingController.createBooking(u2.getId(), bookingRequestDto);
         BookingDto b2 = bookingController.getBookingByID(u2.getId(), b1.getId());
         assertEquals(b1.getId(), b2.getId(), "Ошибка проверки");
+    }
+
+    @Test
+    void approveBooking() {
+        UserDto u1 = new UserDto();
+        u1.setName("USER-1");
+        u1.setEmail("USER-1@mail.ru");
+        u1 = userController.create(u1);
+
+        UserDto u2 = new UserDto();
+        u2.setName("USER-2");
+        u2.setEmail("USER-2@mail.ru");
+        u2 = userController.create(u2);
+
+        ItemDto itemDto = new ItemDto();
+        itemDto.setName("ITEM");
+        itemDto.setDescription("TEXT");
+        itemDto.setAvailable(true);
+        itemDto = itemController.create(u1.getId(), itemDto);
+
+        BookingRequestDto bookingRequestDto = new BookingRequestDto();
+        bookingRequestDto.setItemId(itemDto.getId());
+        bookingRequestDto.setStart(LocalDateTime.now());
+        bookingRequestDto.setEnd(LocalDateTime.now().plusDays(1));
+
+        BookingDto b1 = bookingController.createBooking(u2.getId(), bookingRequestDto);
+        BookingDto b2 = bookingController.approveBooking(u1.getId(), b1.getId(), true);
+        assertEquals(BookingStatus.APPROVED, b2.getStatus(), "Ошибка проверки");
     }
 
 }
