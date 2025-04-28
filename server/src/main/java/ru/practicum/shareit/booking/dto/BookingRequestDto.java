7cdb2f81a071229cd39286a8e4d0088726a4267d
amagmail <skynet@skynet.ru>
2025-04-28 12:38:04 +0300

    Sprint 16. Разработка финального проекта (Убрал лишнюю валидацию с сервера)

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
@@ -2,13 +2,11 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.booking.validators.DateRangeControl;
 
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
-@DateRangeControl
 public class BookingRequestDto {
 
     private Long itemId;
